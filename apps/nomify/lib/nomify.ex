cb3b708d016499dde981876ff2924d6e00c6dc54
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-05-13 14:09:02 -0400

    docs: Application module

--- apps/nomify/lib/nomify.ex
+++ apps/nomify/lib/nomify.ex
@@ -1,9 +1,17 @@
 defmodule Nomify do
   @moduledoc """
-  Nomify keeps the contexts that define your domain
-  and business logic.
+  A document review process to empowering teams to collaborate,  review, and approve documents.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  ## Sub-Domains
+
+  For information on each sub-domain, see:
+
+  - `Nomify.Directory`: Handles the organization and categorization of documents within the system.
+  - `Nomify.Documents`: Manages the lifecycle of documents, including creation, review, and approval processes.
+  - `Nomify.Teams`: Facilitates collaboration by managing team members and their roles in the document review process.
+
+  ## Ecto ERD
+
+  ![Ecto ERD](./assets/erd.png) [View Larger Image](./assets/erd.png)
   """
 end
