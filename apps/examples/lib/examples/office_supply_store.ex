96040229e75c5cca5dff02c404b6e14a9b092e68
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-05-28 15:43:42 -0400

    Trim business customer generated code

--- apps/examples/lib/examples/office_supply_store.ex
+++ apps/examples/lib/examples/office_supply_store.ex
@@ -81,97 +81,17 @@ defmodule Examples.OfficeSupplyStore do
 
   alias Examples.OfficeSupplyStore.BusinessCustomer
 
-  @doc """
-  Returns the list of office_supply_store_business_customers.
-
-  ## Examples
-
-      iex> list_office_supply_store_business_customers()
-      [%BusinessCustomer{}, ...]
-
-  """
-  def list_office_supply_store_business_customers do
-    Repo.all(BusinessCustomer)
-  end
-
-  @doc """
-  Gets a single business_customer.
-
-  Raises `Ecto.NoResultsError` if the Business customer does not exist.
-
-  ## Examples
-
-      iex> get_business_customer!(123)
-      %BusinessCustomer{}
-
-      iex> get_business_customer!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_business_customer!(id), do: Repo.get!(BusinessCustomer, id)
 
-  @doc """
-  Creates a business_customer.
-
-  ## Examples
-
-      iex> create_business_customer(%{field: value})
-      {:ok, %BusinessCustomer{}}
-
-      iex> create_business_customer(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_business_customer(attrs \\ %{}) do
     %BusinessCustomer{}
     |> BusinessCustomer.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a business_customer.
-
-  ## Examples
-
-      iex> update_business_customer(business_customer, %{field: new_value})
-      {:ok, %BusinessCustomer{}}
-
-      iex> update_business_customer(business_customer, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_business_customer(%BusinessCustomer{} = business_customer, attrs) do
     business_customer
     |> BusinessCustomer.changeset(attrs)
     |> Repo.update()
-  end
-
-  @doc """
-  Deletes a business_customer.
-
-  ## Examples
-
-      iex> delete_business_customer(business_customer)
-      {:ok, %BusinessCustomer{}}
-
-      iex> delete_business_customer(business_customer)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_business_customer(%BusinessCustomer{} = business_customer) do
-    Repo.delete(business_customer)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking business_customer changes.
-
-  ## Examples
-
-      iex> change_business_customer(business_customer)
-      %Ecto.Changeset{data: %BusinessCustomer{}}
-
-  """
-  def change_business_customer(%BusinessCustomer{} = business_customer, attrs \\ %{}) do
-    BusinessCustomer.changeset(business_customer, attrs)
   end
 end
