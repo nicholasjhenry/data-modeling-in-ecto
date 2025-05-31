5d45745184de944ed2ee2d626368b9f8a925a037
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-05-31 10:37:13 -0400

    Trim loading bin generated code

--- apps/examples/test/examples/warehouse_test.exs
+++ apps/examples/test/examples/warehouse_test.exs
@@ -70,18 +70,18 @@ defmodule Examples.WarehouseTest do
 
     @invalid_attrs %{size: nil, state: nil, acceptable_temperature_range: nil, designation: nil}
 
-    test "list_warehouse_loading_bins/0 returns all warehouse_loading_bins" do
-      loading_bin = loading_bin_fixture()
-      assert Warehouse.list_warehouse_loading_bins() == [loading_bin]
-    end
-
     test "get_loading_bin!/1 returns the loading_bin with given id" do
       loading_bin = loading_bin_fixture()
       assert Warehouse.get_loading_bin!(loading_bin.id) == loading_bin
     end
 
     test "create_loading_bin/1 with valid data creates a loading_bin" do
-      valid_attrs = %{size: "120.5", state: :empty, acceptable_temperature_range: "some acceptable_temperature_range", designation: :food}
+      valid_attrs = %{
+        size: "120.5",
+        state: :empty,
+        acceptable_temperature_range: "some acceptable_temperature_range",
+        designation: :food
+      }
 
       assert {:ok, %LoadingBin{} = loading_bin} = Warehouse.create_loading_bin(valid_attrs)
       assert loading_bin.size == Decimal.new("120.5")
@@ -96,30 +96,33 @@ defmodule Examples.WarehouseTest do
 
     test "update_loading_bin/2 with valid data updates the loading_bin" do
       loading_bin = loading_bin_fixture()
-      update_attrs = %{size: "456.7", state: :full, acceptable_temperature_range: "some updated acceptable_temperature_range", designation: :toxic_materials}
 
-      assert {:ok, %LoadingBin{} = loading_bin} = Warehouse.update_loading_bin(loading_bin, update_attrs)
+      update_attrs = %{
+        size: "456.7",
+        state: :full,
+        acceptable_temperature_range: "some updated acceptable_temperature_range",
+        designation: :toxic_materials
+      }
+
+      assert {:ok, %LoadingBin{} = loading_bin} =
+               Warehouse.update_loading_bin(loading_bin, update_attrs)
+
       assert loading_bin.size == Decimal.new("456.7")
       assert loading_bin.state == :full
-      assert loading_bin.acceptable_temperature_range == "some updated acceptable_temperature_range"
+
+      assert loading_bin.acceptable_temperature_range ==
+               "some updated acceptable_temperature_range"
+
       assert loading_bin.designation == :toxic_materials
     end
 
     test "update_loading_bin/2 with invalid data returns error changeset" do
       loading_bin = loading_bin_fixture()
-      assert {:error, %Ecto.Changeset{}} = Warehouse.update_loading_bin(loading_bin, @invalid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} =
+               Warehouse.update_loading_bin(loading_bin, @invalid_attrs)
+
       assert loading_bin == Warehouse.get_loading_bin!(loading_bin.id)
-    end
-
-    test "delete_loading_bin/1 deletes the loading_bin" do
-      loading_bin = loading_bin_fixture()
-      assert {:ok, %LoadingBin{}} = Warehouse.delete_loading_bin(loading_bin)
-      assert_raise Ecto.NoResultsError, fn -> Warehouse.get_loading_bin!(loading_bin.id) end
-    end
-
-    test "change_loading_bin/1 returns a loading_bin changeset" do
-      loading_bin = loading_bin_fixture()
-      assert %Ecto.Changeset{} = Warehouse.change_loading_bin(loading_bin)
     end
   end
 end
