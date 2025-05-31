a01827eec0014dccb8b66f10b169982a210e47a2
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-05-31 10:33:37 -0400

    Trim loading area generated code

--- apps/examples/lib/examples/warehouse.ex
+++ apps/examples/lib/examples/warehouse.ex
@@ -8,97 +8,17 @@ defmodule Examples.Warehouse do
 
   alias Examples.Warehouse.LoadingArea
 
-  @doc """
-  Returns the list of warehouse_loading_areas.
-
-  ## Examples
-
-      iex> list_warehouse_loading_areas()
-      [%LoadingArea{}, ...]
-
-  """
-  def list_warehouse_loading_areas do
-    Repo.all(LoadingArea)
-  end
-
-  @doc """
-  Gets a single loading_area.
-
-  Raises `Ecto.NoResultsError` if the Loading area does not exist.
-
-  ## Examples
-
-      iex> get_loading_area!(123)
-      %LoadingArea{}
-
-      iex> get_loading_area!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_loading_area!(id), do: Repo.get!(LoadingArea, id)
 
-  @doc """
-  Creates a loading_area.
-
-  ## Examples
-
-      iex> create_loading_area(%{field: value})
-      {:ok, %LoadingArea{}}
-
-      iex> create_loading_area(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_loading_area(attrs \\ %{}) do
     %LoadingArea{}
     |> LoadingArea.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a loading_area.
-
-  ## Examples
-
-      iex> update_loading_area(loading_area, %{field: new_value})
-      {:ok, %LoadingArea{}}
-
-      iex> update_loading_area(loading_area, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_loading_area(%LoadingArea{} = loading_area, attrs) do
     loading_area
     |> LoadingArea.changeset(attrs)
     |> Repo.update()
-  end
-
-  @doc """
-  Deletes a loading_area.
-
-  ## Examples
-
-      iex> delete_loading_area(loading_area)
-      {:ok, %LoadingArea{}}
-
-      iex> delete_loading_area(loading_area)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_loading_area(%LoadingArea{} = loading_area) do
-    Repo.delete(loading_area)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking loading_area changes.
-
-  ## Examples
-
-      iex> change_loading_area(loading_area)
-      %Ecto.Changeset{data: %LoadingArea{}}
-
-  """
-  def change_loading_area(%LoadingArea{} = loading_area, attrs \\ %{}) do
-    LoadingArea.changeset(loading_area, attrs)
   end
 end
--- apps/examples/test/examples/warehouse_test.exs
+++ apps/examples/test/examples/warehouse_test.exs
@@ -10,18 +10,18 @@ defmodule Examples.WarehouseTest do
 
     @invalid_attrs %{size: nil, type: nil, state: nil, average_temperature: nil}
 
-    test "list_warehouse_loading_areas/0 returns all warehouse_loading_areas" do
-      loading_area = loading_area_fixture()
-      assert Warehouse.list_warehouse_loading_areas() == [loading_area]
-    end
-
     test "get_loading_area!/1 returns the loading_area with given id" do
       loading_area = loading_area_fixture()
       assert Warehouse.get_loading_area!(loading_area.id) == loading_area
     end
 
     test "create_loading_area/1 with valid data creates a loading_area" do
-      valid_attrs = %{size: "120.5", type: :room_temperature, state: :static, average_temperature: "120.5"}
+      valid_attrs = %{
+        size: "120.5",
+        type: :room_temperature,
+        state: :static,
+        average_temperature: "120.5"
+      }
 
       assert {:ok, %LoadingArea{} = loading_area} = Warehouse.create_loading_area(valid_attrs)
       assert loading_area.size == Decimal.new("120.5")
@@ -36,9 +36,17 @@ defmodule Examples.WarehouseTest do
 
     test "update_loading_area/2 with valid data updates the loading_area" do
       loading_area = loading_area_fixture()
-      update_attrs = %{size: "456.7", type: :refrigerated, state: :receiving, average_temperature: "456.7"}
 
-      assert {:ok, %LoadingArea{} = loading_area} = Warehouse.update_loading_area(loading_area, update_attrs)
+      update_attrs = %{
+        size: "456.7",
+        type: :refrigerated,
+        state: :receiving,
+        average_temperature: "456.7"
+      }
+
+      assert {:ok, %LoadingArea{} = loading_area} =
+               Warehouse.update_loading_area(loading_area, update_attrs)
+
       assert loading_area.size == Decimal.new("456.7")
       assert loading_area.type == :refrigerated
       assert loading_area.state == :receiving
@@ -47,19 +55,11 @@ defmodule Examples.WarehouseTest do
 
     test "update_loading_area/2 with invalid data returns error changeset" do
       loading_area = loading_area_fixture()
-      assert {:error, %Ecto.Changeset{}} = Warehouse.update_loading_area(loading_area, @invalid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} =
+               Warehouse.update_loading_area(loading_area, @invalid_attrs)
+
       assert loading_area == Warehouse.get_loading_area!(loading_area.id)
-    end
-
-    test "delete_loading_area/1 deletes the loading_area" do
-      loading_area = loading_area_fixture()
-      assert {:ok, %LoadingArea{}} = Warehouse.delete_loading_area(loading_area)
-      assert_raise Ecto.NoResultsError, fn -> Warehouse.get_loading_area!(loading_area.id) end
-    end
-
-    test "change_loading_area/1 returns a loading_area changeset" do
-      loading_area = loading_area_fixture()
-      assert %Ecto.Changeset{} = Warehouse.change_loading_area(loading_area)
     end
   end
 end
