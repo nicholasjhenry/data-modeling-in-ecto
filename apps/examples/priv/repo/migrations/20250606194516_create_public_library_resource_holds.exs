946eb48ad3b395b903807e2a889e428b93a467d3
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-06-06 16:43:20 -0400

    Add missing resource hold references

--- apps/examples/priv/repo/migrations/20250606194516_create_public_library_resource_holds.exs
+++ apps/examples/priv/repo/migrations/20250606194516_create_public_library_resource_holds.exs
@@ -5,6 +5,9 @@ defmodule Examples.Repo.Migrations.CreatePublicLibraryResourceHolds do
     create table(:public_library_resource_holds) do
       add :type, :string
       add :permit_resource_fees, :boolean, default: false, null: false
+      add :patron_id, references(:public_library_patrons, on_delete: :nothing)
+      add :resource_id, references(:public_library_resources, on_delete: :nothing)
+      add :branch_id, references(:public_library_branches, on_delete: :nothing)
 
       timestamps()
     end
