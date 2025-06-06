463ae0e0434d75b883bde4ea0353bbffc8c6b966
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-06-06 13:04:48 -0400

    Trim library branch generated code

--- apps/examples/test/examples/public_library_test.exs
+++ apps/examples/test/examples/public_library_test.exs
@@ -10,11 +10,6 @@ defmodule Examples.PublicLibraryTest do
 
     @invalid_attrs %{name: nil}
 
-    test "list_public_library_branches/0 returns all public_library_branches" do
-      branch = branch_fixture()
-      assert PublicLibrary.list_public_library_branches() == [branch]
-    end
-
     test "get_branch!/1 returns the branch with given id" do
       branch = branch_fixture()
       assert PublicLibrary.get_branch!(branch.id) == branch
@@ -29,31 +24,6 @@ defmodule Examples.PublicLibraryTest do
 
     test "create_branch/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = PublicLibrary.create_branch(@invalid_attrs)
-    end
-
-    test "update_branch/2 with valid data updates the branch" do
-      branch = branch_fixture()
-      update_attrs = %{name: "some updated name"}
-
-      assert {:ok, %Branch{} = branch} = PublicLibrary.update_branch(branch, update_attrs)
-      assert branch.name == "some updated name"
-    end
-
-    test "update_branch/2 with invalid data returns error changeset" do
-      branch = branch_fixture()
-      assert {:error, %Ecto.Changeset{}} = PublicLibrary.update_branch(branch, @invalid_attrs)
-      assert branch == PublicLibrary.get_branch!(branch.id)
-    end
-
-    test "delete_branch/1 deletes the branch" do
-      branch = branch_fixture()
-      assert {:ok, %Branch{}} = PublicLibrary.delete_branch(branch)
-      assert_raise Ecto.NoResultsError, fn -> PublicLibrary.get_branch!(branch.id) end
-    end
-
-    test "change_branch/1 returns a branch changeset" do
-      branch = branch_fixture()
-      assert %Ecto.Changeset{} = PublicLibrary.change_branch(branch)
     end
   end
 end
