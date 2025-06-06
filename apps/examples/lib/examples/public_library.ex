2b52a44c0cd0d32d0162a8da25a16b3c8fdb91f8
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-06-06 15:47:31 -0400

    Trim resource hold generated code

--- apps/examples/lib/examples/public_library.ex
+++ apps/examples/lib/examples/public_library.ex
@@ -62,97 +62,11 @@ defmodule Examples.PublicLibrary do
 
   alias Examples.PublicLibrary.ResourceHold
 
-  @doc """
-  Returns the list of public_library_resource_holds.
-
-  ## Examples
-
-      iex> list_public_library_resource_holds()
-      [%ResourceHold{}, ...]
-
-  """
-  def list_public_library_resource_holds do
-    Repo.all(ResourceHold)
-  end
-
-  @doc """
-  Gets a single resource_hold.
-
-  Raises `Ecto.NoResultsError` if the Resource hold does not exist.
-
-  ## Examples
-
-      iex> get_resource_hold!(123)
-      %ResourceHold{}
-
-      iex> get_resource_hold!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_resource_hold!(id), do: Repo.get!(ResourceHold, id)
 
-  @doc """
-  Creates a resource_hold.
-
-  ## Examples
-
-      iex> create_resource_hold(%{field: value})
-      {:ok, %ResourceHold{}}
-
-      iex> create_resource_hold(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_resource_hold(attrs \\ %{}) do
     %ResourceHold{}
     |> ResourceHold.changeset(attrs)
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a resource_hold.
-
-  ## Examples
-
-      iex> update_resource_hold(resource_hold, %{field: new_value})
-      {:ok, %ResourceHold{}}
-
-      iex> update_resource_hold(resource_hold, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_resource_hold(%ResourceHold{} = resource_hold, attrs) do
-    resource_hold
-    |> ResourceHold.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a resource_hold.
-
-  ## Examples
-
-      iex> delete_resource_hold(resource_hold)
-      {:ok, %ResourceHold{}}
-
-      iex> delete_resource_hold(resource_hold)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_resource_hold(%ResourceHold{} = resource_hold) do
-    Repo.delete(resource_hold)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking resource_hold changes.
-
-  ## Examples
-
-      iex> change_resource_hold(resource_hold)
-      %Ecto.Changeset{data: %ResourceHold{}}
-
-  """
-  def change_resource_hold(%ResourceHold{} = resource_hold, attrs \\ %{}) do
-    ResourceHold.changeset(resource_hold, attrs)
   end
 end
