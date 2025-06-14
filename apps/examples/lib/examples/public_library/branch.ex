f5038f91e72ed678cfe5d79c5d175917010bbd35
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-06-13 20:58:36 -0400

    Add resource hold relevant fields to library branch

--- apps/examples/lib/examples/public_library/branch.ex
+++ apps/examples/lib/examples/public_library/branch.ex
@@ -4,6 +4,15 @@ defmodule Examples.PublicLibrary.Branch do
 
   schema "public_library_branches" do
     field :name, :string
+    field :permitted_resource_holds, {:array, :string}, default: ["open", "closed"]
+
+    field :business_days, {:array, :string},
+      default: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    field :state, Ecto.Enum, values: [:normal, :reviewing_inventory], default: :normal
+
+    field :permitted_patron_roles_for_resource_holds, {:array, :string},
+      default: ["regular", "researcher"]
 
     timestamps()
   end
