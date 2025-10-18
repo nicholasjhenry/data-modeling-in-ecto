8bd53cd089486395c412a811c2cd5c0407b3c5fc
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-10-18 15:03:16 -0400

    Add setup tasks

--- apps/examples/mix.exs
+++ apps/examples/mix.exs
@@ -49,7 +49,10 @@ defmodule Examples.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: []
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 
